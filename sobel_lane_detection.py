8a676a50ee39646bd24a28d4531c5106828ca6be
furqi98 <148366759+furqi98@users.noreply.github.com>
2025-02-06 14:27:35 +0500

    Create sobel_lane_detection.py

---
+++ sobel_lane_detection.py
@@ -0,0 +1,4 @@
+**Sobel Filter for Lane Detection**
+   - Dataset: [Udacity Self-Driving Car](https://github.com/udacity/self-driving-car)
+   - Use Case: Detecting lane boundaries in **autonomous driving**
+   - Script: `sobel_lane_detection.py`
